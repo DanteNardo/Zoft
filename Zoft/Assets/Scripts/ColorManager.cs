065967ad8d5fce6b2ffd2ddb0ce3af80158e8f5d
Dantenardo <dante.nardo@outlook.com>
2020-04-30 13:27:25 -0700

    Summary: Dropdown for spawning and color picker Detailed: Updated buttons to spawn objects to instead use a dropdown and added a new input field to update the color of spawned objects.

---
+++ Zoft/Assets/Scripts/ColorManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour {
+    #region Members
+    public Color polygonColor;
+	#endregion
+
+	#region Methods
+	public void SetColor(string hex) {
+		bool passed = ColorUtility.TryParseHtmlString(hex, out polygonColor);
+		if (passed == false) {
+			Debug.LogError("Invalid HTML Hex String");
+			polygonColor = Color.red;
+		}
+	}
+	#endregion
+}
--- Zoft/Assets/Scripts/ShapeSpawner.cs
+++ Zoft/Assets/Scripts/ShapeSpawner.cs
@@ -1,17 +1,56 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ShapeSpawner : MonoBehaviour {
     #region Members
+    public ColorManager colorManager;
     public GameObject shapePrefab;
     private GameObject lastInstance;
     #endregion
 
     #region Methods
+    public void Spawn(Text text) {
+        Spawn(text.text);
+    }
+
+    public void Spawn(string shapeName) {
+        switch (shapeName) {
+            case "Triangle": 
+                Spawn(3);
+                break;
+            case "Square":
+                Spawn(4);
+                break;
+            case "Pentagon":
+                Spawn(5);
+                break;
+            case "Hexagon":
+                Spawn(6);
+                break;
+            case "Heptagon":
+                Spawn(7);
+                break;
+            case "Octagon":
+                Spawn(8);
+                break;
+            default:
+                Debug.LogError("Incorrect string passed to Spawn");
+                break;
+        }
+    }
+
     public void Spawn(int verticeCount) {
+        // Handle spawning instances
         if (lastInstance != null) Destroy(lastInstance);
         lastInstance = Instantiate(shapePrefab, Vector3.zero, Quaternion.identity);
-        Polygon poly = lastInstance.GetComponent<Polygon>();
-        poly.count = verticeCount;
+
+        // Get the polygon and update vertex count
+        Polygon polygon = lastInstance.GetComponent<Polygon>();
+        polygon.count = verticeCount;
+
+        // Get the component to draw and update the color
+        DrawPolygon draw = lastInstance.GetComponent<DrawPolygon>();
+        draw.polygonColor = colorManager.polygonColor;
     }
     #endregion
 }
